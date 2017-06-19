2e290e564c5ed08e69584bcbdba54fce1b338fff
ccwrc <j.muszelik@gmail.com>
2017-06-19 17:48:15 +0200

    generate user entity

---
+++ truck/src/TruckBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TruckBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="TruckBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
