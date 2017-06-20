52ad1c8226b42953aed2a05bed0344f806176175
ccwrc <j.muszelik@gmail.com>
2017-06-20 12:04:04 +0200

    fit user class to fos_user

--- truck/src/TruckBundle/Entity/User.php
+++ truck/src/TruckBundle/Entity/User.php
@@ -2,33 +2,24 @@
 
 namespace TruckBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user")
- * @ORM\Entity(repositoryClass="TruckBundle\Repository\UserRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class User
-{
+class User extends BaseUser {
+
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
+    public function __construct() {
+        parent::__construct();
     }
+
 }
