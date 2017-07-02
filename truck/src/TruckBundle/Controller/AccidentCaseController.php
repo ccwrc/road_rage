cf5c7e21e28c6ff59a304b6fc586eaf2cfd992f9
ccwrc <j.muszelik@gmail.com>
2017-07-02 13:02:54 +0200

    add security to accident case controller

--- truck/src/TruckBundle/Controller/AccidentCaseController.php
+++ truck/src/TruckBundle/Controller/AccidentCaseController.php
@@ -4,9 +4,13 @@ namespace TruckBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 use TruckBundle\Entity\AccidentCase;
 
+/**
+ * @Security("has_role('ROLE_OPERATOR')")
+ */
 class AccidentCaseController extends Controller
 {
     /**
