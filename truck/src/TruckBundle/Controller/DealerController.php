2f522e0ea4ce78bc5f6927d8695e2a1f9e37a6d4
ccwrc <j.muszelik@gmail.com>
2017-07-03 13:17:45 +0200

    add show dealer + cosmetology

--- truck/src/TruckBundle/Controller/DealerController.php
+++ truck/src/TruckBundle/Controller/DealerController.php
@@ -12,26 +12,36 @@ use TruckBundle\Entity\Dealer;
  * @Route("/dealer")
  * @Security("has_role('ROLE_DEALER')")
  */
-class DealerController extends Controller
-{
+class DealerController extends Controller {
+    
     /**
      * @Route("/testDealer")
      */
-    public function testDealerAction()
-    {
+    public function testDealerAction() {
         return $this->render('TruckBundle:Dealer:test_dealer.html.twig', array(
-            // ...
+                        // ...
         ));
     }
-    
+
     /**
      * @Route("/showAllDealers")
      */
     public function showAllDealersAction() {
         $dealers = $this->getDoctrine()->getRepository("TruckBundle:Dealer")->findAll();
-        
+
         return $this->render('TruckBundle:Dealer:show_all_dealers.html.twig', [
-            "dealers" => $dealers
+                    "dealers" => $dealers
+        ]);
+    }
+
+    /**
+     * @Route("/showDealer/{id}", requirements={"id"="\d+"})
+     */
+    public function showDealerAction($id) {
+        $dealer = $this->getDoctrine()->getRepository("TruckBundle:Dealer")->find($id);
+
+        return $this->render('TruckBundle:Dealer:show_dealer.html.twig', [
+                    "dealer" => $dealer
         ]);
     }
 
