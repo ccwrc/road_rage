0d6f746ead05936a726d7a2dbe754a1f874c1a8f
ccwrc <j.muszelik@gmail.com>
2017-07-12 12:58:17 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # road_rage
+
+#
+early beta - operator panel: 
+![Alt text](https://images86.fotosik.pl/266/646603a4eece6f5c.png "beta - operator panel")
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-phpinfo();
-
-/*     public function __construct($environment, $debug) {
-        date_default_timezone_set('Europe/Warsaw');
-        parent::__construct($environment, $debug);
-    }
- * appkernel.php */
