21c03d8a366d7590bb504adb447835de2a015577
ccwrc <j.muszelik@gmail.com>
2017-07-13 13:20:59 +0200

    add request (form) to vehicle controller

--- truck/src/TruckBundle/Controller/VehicleController.php
+++ truck/src/TruckBundle/Controller/VehicleController.php
@@ -5,6 +5,7 @@ namespace TruckBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 use TruckBundle\Entity\Vehicle;
 use TruckBundle\Form\VehicleType;
@@ -18,10 +19,11 @@ class VehicleController extends Controller {
      * @Route("/createVehicle")
      */
     public function createVehicleAction() {
-        //
+        $vehicle = new Vehicle();
+        $form = $this->createForm(VehicleType::class, $vehicle);
 
         return $this->render('TruckBundle:Vehicle:create_vehicle.html.twig', [
-                        //
+                       "form" => $form->createView()
         ]);
     }
 
