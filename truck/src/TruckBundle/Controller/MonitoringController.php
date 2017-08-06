718a94ee9fbd6190b2066b6cc7fd6957a5d287c5
ccwrc <j.muszelik@gmail.com>
2017-08-06 16:17:52 +0200

    connect form to monitoring controller, add request component

--- truck/src/TruckBundle/Controller/MonitoringController.php
+++ truck/src/TruckBundle/Controller/MonitoringController.php
@@ -5,8 +5,12 @@ namespace TruckBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 use TruckBundle\Entity\Monitoring;
+use TruckBundle\Entity\AccidentCase;
+use TruckBundle\Form\Monitoring\MonitoringType;
+// use TruckBundle\Entity\User;
 
 /**
  * @Route("/monitoring")
@@ -40,7 +44,24 @@ class MonitoringController extends Controller {
      * @Route("/{caseId}/createMonitoring", requirements={"caseId"="\d+"})
      */
     public function createMonitoringAction(Request $req, $caseId) {
-        // form !
+        // $user = $this->container->get("security.context")->getToken()->getUser();
+        $monitoring = new Monitoring();
+        $case = $this->getDoctrine()->getRepository("TruckBundle:AccidentCase")->find($caseId);
+        $monitoring->setAccidentCase($case);
+        $form = $this->createForm(MonitoringType::class, $monitoring);
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $monitoring = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($monitoring);
+            $em->flush();
+
+            return $this->redirectToRoute("truck_operator_panel", [
+                        "caseId" => $caseId
+            ]);
+        }
 
         return $this->render('TruckBundle:Monitoring:create_monitoring.html.twig', [
                     "form" => $form->createView()
---
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TruckBundle\Form\Monitoring;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonitoringType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("code", "text", ["label" => "Code: ", "data" => "testCode"])
+                ->add("codeDescription", "text", ["label" => "code description: "])
+                ->add("operator", "text", ["label" => "test operator: "])
+                ->add("timeSave", "datetime", ["label" => "time save: "])
+                ->add("timeSet", "datetime", ["label" => "time set: "])
+                ->add("document", "hidden", ["label" => "document: "])
+                ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("comments", "textarea", ["label" => "comments: "])
+                ->add("outComment", "hidden", ["label" => "outComment: "])
+                ->add("homeDealerId", "hidden", ["label" => "homeDealerId: "])
+                ->add("repairDealerId", "hidden", ["label" => "repairDealerId: "])
+                ->add("contactMail", "hidden", ["label" => "contactMail: "])
+                ->add("optionalMails", "text", ["label" => "optionalMails: "]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class" => "TruckBundle\Entity\Monitoring"
+        ]);
+    }
+
+}
