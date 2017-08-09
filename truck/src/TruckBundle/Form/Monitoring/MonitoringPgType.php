fb5a6b3b95b324488519e5a24cf69b674e450029
ccwrc <j.muszelik@gmail.com>
2017-08-09 12:19:44 +0200

    add MonitoringPgType

---
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringPgType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TruckBundle\Form\Monitoring;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonitoringPgType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("code", "hidden", ["data" => "PG"])
+                // TODO timeSave -> auto in controller
+                // ->add("timeSave", "datetime", ["label" => "time save: "])
+                // ->add("timeSet", "datetime", ["label" => "time set: "])
+                // ->add("document", "hidden", ["label" => "document: "])
+                ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("comments", "textarea", ["label" => "comments: "])
+                //TODO out comment for document
+                ->add("outComment", "hidden", ["label" => "outComment: "]);
+                //TODO contact mail/opt mail for document
+                // ->add("contactMail", "hidden", ["label" => "contactMail: "])
+                //->add("optionalMails", "text", ["label" => "optionalMails: "]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class" => "TruckBundle\Entity\Monitoring"
+        ]);
+    }
+
+}
