8749d0bc6872e98348a6c17804dbd011a7d8ac5d
ccwrc <j.muszelik@gmail.com>
2017-08-13 12:01:06 +0200

    add MonitoringCpgEditType

---
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringCpgEditType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TruckBundle\Form\Monitoring;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonitoringCpgEditType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("comments", "textarea", ["label" => "Comments: "]);
+        //TODO out comment for document
+        //TODO contact mail/opt mail for document
+        // ->add("contactMail", "hidden", ["label" => "contactMail: "])
+        //->add("optionalMails", "text", ["label" => "optionalMails: "]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class" => "TruckBundle\Entity\Monitoring"
+        ]);
+    }
+
+}
