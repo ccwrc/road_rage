31753dd532bba9d8de05bc0e62f2d677909246ed
ccwrc <j.muszelik@gmail.com>
2017-08-14 11:40:05 +0200

    connect MonitoringIncomingEditType to controller

--- truck/src/TruckBundle/Controller/MonitoringController.php
+++ truck/src/TruckBundle/Controller/MonitoringController.php
@@ -15,6 +15,7 @@ use TruckBundle\Form\Monitoring\MonitoringPgEditType;
 use TruckBundle\Form\Monitoring\MonitoringCpgType;
 use TruckBundle\Form\Monitoring\MonitoringCpgEditType;
 use TruckBundle\Form\Monitoring\MonitoringIncomingType;
+use TruckBundle\Form\Monitoring\MonitoringIncomingEditType;
 use \DateTime;
 
 /**
