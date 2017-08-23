d508f10711971aa98307e8faedf2f3a07b830962
ccwrc <j.muszelik@gmail.com>
2017-08-23 11:50:15 +0200

    connect MonitoringOutEditType to controller

--- truck/src/TruckBundle/Controller/MonitoringController.php
+++ truck/src/TruckBundle/Controller/MonitoringController.php
@@ -22,6 +22,7 @@ use TruckBundle\Form\Monitoring\MonitoringStrrEditType;
 use TruckBundle\Form\Monitoring\MonitoringIncomingType;
 use TruckBundle\Form\Monitoring\MonitoringIncomingEditType;
 use TruckBundle\Form\Monitoring\MonitoringOutType;
+use TruckBundle\Form\Monitoring\MonitoringOutEditType;
 use \DateTime;
 
 /**
