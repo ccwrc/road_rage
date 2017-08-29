1fdab683a7b571aca0ac1cfcd436fbe04736ed3a
ccwrc <j.muszelik@gmail.com>
2017-08-29 13:14:50 +0200

    create MonitoringOutController

--- truck/src/TruckBundle/Controller/MonitoringController.php
+++ truck/src/TruckBundle/Controller/MonitoringController.php
@@ -21,8 +21,8 @@ use TruckBundle\Entity\AccidentCase;
 //use TruckBundle\Form\Monitoring\MonitoringStrrEditType;
 //use TruckBundle\Form\Monitoring\MonitoringIncomingType;
 //use TruckBundle\Form\Monitoring\MonitoringIncomingEditType;
-use TruckBundle\Form\Monitoring\MonitoringOutType;
-use TruckBundle\Form\Monitoring\MonitoringOutEditType;
+//use TruckBundle\Form\Monitoring\MonitoringOutType;
+//use TruckBundle\Form\Monitoring\MonitoringOutEditType;
 use TruckBundle\Form\Monitoring\MonitoringEndType;
 use TruckBundle\Form\Monitoring\MonitoringEndEditType;
 use \DateTime;  // to delete after refactor
@@ -58,11 +58,9 @@ class MonitoringController extends Controller {
      */
     public function monitoringCodesManualAction() {
 
-        return $this->render('TruckBundle:Monitoring:monitoring_codes_manual.html.twig', [
-                        //...
-        ]);
+        return $this->render('TruckBundle:Monitoring:monitoring_codes_manual.html.twig');
     }
-    
+
 //    /**
 //     * @Route("/{caseId}/createMonitoringPg", requirements={"caseId"="\d+"})
 //     */
@@ -412,63 +410,63 @@ class MonitoringController extends Controller {
 //        ]);
 //    }   
     
-    /**
-     * @Route("/{caseId}/createMonitoringOut", requirements={"caseId"="\d+"})
-     */
-    public function createMonitoringOutAction(Request $req, $caseId) {
-        $operatorName = $this->getOperatorName();
-        $case = $this->getDoctrine()->getRepository("TruckBundle:AccidentCase")->find($caseId);
-
-        $monitoring = new Monitoring();
-        $monitoring->setAccidentCase($case)->setOperator($operatorName)
-                ->setTimeSave(new DateTime("now"))->setCode("Out");
-        $form = $this->createForm(MonitoringOutType::class, $monitoring);
-
-        $form->handleRequest($req);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $monitoring = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($monitoring);
-            $em->flush();
-
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
-            ]);
-        }
-
-        return $this->render('TruckBundle:Monitoring:create_monitoring_out.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
-        ]);
-    }
+//    /**
+//     * @Route("/{caseId}/createMonitoringOut", requirements={"caseId"="\d+"})
+//     */
+//    public function createMonitoringOutAction(Request $req, $caseId) {
+//        $operatorName = $this->getOperatorName();
+//        $case = $this->getDoctrine()->getRepository("TruckBundle:AccidentCase")->find($caseId);
+//
+//        $monitoring = new Monitoring();
+//        $monitoring->setAccidentCase($case)->setOperator($operatorName)
+//                ->setTimeSave(new DateTime("now"))->setCode("Out");
+//        $form = $this->createForm(MonitoringOutType::class, $monitoring);
+//
+//        $form->handleRequest($req);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $monitoring = $form->getData();
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($monitoring);
+//            $em->flush();
+//
+//            return $this->redirectToRoute("truck_operator_panel", [
+//                        "caseId" => $caseId
+//            ]);
+//        }
+//
+//        return $this->render('TruckBundle:Monitoring:create_monitoring_out.html.twig', [
+//                    "form" => $form->createView(),
+//                    "caseId" => $caseId
+//        ]);
+//    }
     
-    /**
-     * @Route("/{monitoringId}/editMonitoringOut", requirements={"monitoringId"="\d+"})
-     */
-    public function editMonitoringOutAction(Request $req, $monitoringId) {
-        $monitoring = $this->getDoctrine()->getRepository("TruckBundle:Monitoring")
-                ->find($monitoringId);
-        $caseId = $monitoring->getAccidentCase()->getId();
-        $form = $this->createForm(MonitoringOutEditType::class, $monitoring);
-
-        $form->handleRequest($req);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $monitoring = $form->getData();
-            $operatorName = $this->getOperatorName();
-            $monitoring->setOperator($operatorName);
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
-
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
-            ]);
-        }
-
-        return $this->render('TruckBundle:Monitoring:edit_monitoring_out.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
-        ]);
-    }
+//    /**
+//     * @Route("/{monitoringId}/editMonitoringOut", requirements={"monitoringId"="\d+"})
+//     */
+//    public function editMonitoringOutAction(Request $req, $monitoringId) {
+//        $monitoring = $this->getDoctrine()->getRepository("TruckBundle:Monitoring")
+//                ->find($monitoringId);
+//        $caseId = $monitoring->getAccidentCase()->getId();
+//        $form = $this->createForm(MonitoringOutEditType::class, $monitoring);
+//
+//        $form->handleRequest($req);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $monitoring = $form->getData();
+//            $operatorName = $this->getOperatorName();
+//            $monitoring->setOperator($operatorName);
+//            $em = $this->getDoctrine()->getManager();
+//            $em->flush();
+//
+//            return $this->redirectToRoute("truck_operator_panel", [
+//                        "caseId" => $caseId
+//            ]);
+//        }
+//
+//        return $this->render('TruckBundle:Monitoring:edit_monitoring_out.html.twig', [
+//                    "form" => $form->createView(),
+//                    "caseId" => $caseId
+//        ]);
+//    }
     
     /**
      * @Route("/{caseId}/createMonitoringEnd", requirements={"caseId"="\d+"})
