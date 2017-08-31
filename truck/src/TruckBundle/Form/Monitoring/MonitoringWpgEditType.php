ed4f47cf3311bc5b2e49b7d4a26cc732254a2d36
ccwrc <j.muszelik@gmail.com>
2017-08-31 15:42:27 +0200

    add MonitoringWpgEditType

---
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringWpgEditType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TruckBundle\Form\Monitoring;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonitoringWpgEditType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("comments", "textarea", ["label" => "Comments: "]);
+        //TODO out comment for document
+        //out comment -> sorry, document already sent
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class" => "TruckBundle\Entity\Monitoring"
+        ]);
+    }
+
+}
