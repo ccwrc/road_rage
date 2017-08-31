5863a4eba9c4c5afe3623c717e94cb41d05bced6
ccwrc <j.muszelik@gmail.com>
2017-08-31 12:56:23 +0200

    add createMonitoringWpgAction

--- truck/src/TruckBundle/Controller/MonitoringWpgController.php
+++ truck/src/TruckBundle/Controller/MonitoringWpgController.php
@@ -16,7 +16,38 @@ use \DateTime;
  */
 class MonitoringEndController extends MonitoringController {
 
+    /**
+     * @Route("/{monitoringPgId}/createMonitoringWpg", requirements={"monitoringPgId"="\d+"})
+     */
+    public function createMonitoringWpgAction(Request $req, $monitoringId) {
+        $operatorName = $this->getOperatorName();
+        $monitoringPg = $this->getDoctrine()->getRepository("TruckBundle:Monitoring")
+                ->find($monitoringPgId);
+        $case = $monitoringPg->getAccidentCase();
+        $caseId = $case->getId();
+        $homeDealer = $monitoringPg->getHomeDealer();
 
+        $monitoringWpg = new Monitoring();
+        $monitoringWpg->setAccidentCase($case)->setOperator($operatorName)->setHomeDealer($homeDealer)
+                ->setTimeSave(new DateTime("now"))->setCode("WPG");
+        $form = $this->createForm(MonitoringWpgType::class, $monitoringWpg);
 
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $monitoringWpg = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($monitoringWpg);
+            $em->flush();
+
+            return $this->redirectToRoute("truck_operator_panel", [
+                        "caseId" => $caseId
+            ]);
+        }
+
+        return $this->render('TruckBundle:Monitoring:create_monitoring_wpg.html.twig', [
+                    "form" => $form->createView(),
+                    "caseId" => $caseId
+        ]);
+    }
 
 }
