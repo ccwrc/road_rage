b08d63e3419970d776fba00d05f3915d3c2d1738
ccwrc <j.muszelik@gmail.com>
2017-09-01 13:59:45 +0200

    create MonitoringWcpgType

---
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringWcpgType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TruckBundle\Form\Monitoring;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonitoringWcpgType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("comments", "textarea", ["label" => "Comments: "]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class" => "TruckBundle\Entity\Monitoring"
+        ]);
+    }
+
+}
