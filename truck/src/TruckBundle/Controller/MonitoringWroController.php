ac25f83627b656cd9894a096fd0952acbe865335
ccwrc <j.muszelik@gmail.com>
2017-09-03 11:54:09 +0200

    create MonitoringWroController

---
+++ truck/src/TruckBundle/Controller/MonitoringWroController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TruckBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+use TruckBundle\Entity\Monitoring;
+
+use \DateTime;
+
+/**
+ * @Route("/monitoring")
+ * @Security("has_role('ROLE_OPERATOR')")
+ */
+class MonitoringWroController extends MonitoringController {
+
+
+    
+
+}
