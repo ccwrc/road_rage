dd19a274e952d367899e1f74fe4cd5cb6125d1c5
ccwrc <j.muszelik@gmail.com>
2017-09-04 08:57:50 +0200

    dump db

--- dump_fake_db/4.09.2017.truck.sql
+++ dump_fake_db/4.09.2017.truck.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 08, 2017 at 06:22 PM
--- Server version: 5.7.19-0ubuntu0.16.04.1
--- PHP Version: 7.0.15-0ubuntu0.16.04.4
+-- Czas generowania: 04 Wrz 2017, 08:57
+-- Wersja serwera: 5.7.19-0ubuntu0.16.04.1
+-- Wersja PHP: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `truck`
+-- Baza danych: `truck`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accident_case`
+-- Struktura tabeli dla tabeli `accident_case`
 --
 
 CREATE TABLE `accident_case` (
@@ -47,24 +47,25 @@ CREATE TABLE `accident_case` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `accident_case`
+-- Zrzut danych tabeli `accident_case`
 --
 
 INSERT INTO `accident_case` (`id`, `damage_description`, `location`, `driver_contact`, `comment`, `info_sms`, `info_mail`, `status`, `progress`, `report_late`, `report_rs_time`, `report_nrs_time`, `report_repair_total`, `report_arrival_time`, `report_case_total`, `report_repair_status`, `vehicle_id`) VALUES
 (1, 'opis uszkodzenia edit', 'lokacja w miescie edit', 'kontakt do kierowcy 444 555 555', 'zwykly komentarza', 'edit', 'edit', 'active', 'start', 11, 0, 666, 0, 0, 0, 'in progress', 1),
-(2, 'opis2 uszkodzenia2', 'lokacja w miesci22e', 'kontakt do kie22rowcy 444 555 555', 'zwykly22 komentarza', NULL, NULL, 'active', 'start', 0, 0, 0, 0, 0, 0, NULL, 1),
-(3, 'sdfsfd', 'sdfs', 'sdf', 'empty comment', 'sdfs', 'dsf', 'active', 'start', 0, 0, 0, 0, 0, 0, '0', 1),
+(2, 'opis2 uszkodzenia2', 'lokacja w miesci22e', 'kontakt do kie22rowcy 444 555 555', 'zwykly22 komentarza', NULL, NULL, 'active', 'start', 123, 0, 0, 0, 0, 0, 'dfg', 1),
+(3, 'sdfsfd', 'sdfs', 'sdf', 'empty comment', 'sdfs', 'dsf', 'active', 'start', 0, 1111, 0, 0, 0, 0, '0', 1),
 (4, 'damage description', 'warsaw', 'jan99999999', 'start case', 'empty', 'empty', 'active', 'start', 0, 0, 0, 0, 0, 0, '0', 3),
 (6, 'po prostu awarisa', '333w mieście', 'jan jankowski 123 456 789', 'zgloszenie awarii', 'brak', 'brak', 'active', 'start', 0, 0, 0, 33, 33, 33, 'notification', 11),
-(7, 'sagds', 'sd', 'sdg', 'sdg', 'sdg', 'sdg', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 2),
+(7, 'sagds', 'sd', 'sdg', 'sdg', 'sdg', 'sdg', 'active', 'start', 0, 0, 11, 0, 0, 0, 'notification', 2),
 (15, 'wer', 'wer', 'wer', 'werwerwrewrr', 'wer', 'wer', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 13),
 (16, 'new date test', 'new date test', 'new date test', 'new date test', 'new date test', 'new date test', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 6),
-(17, 'sedg', 'dsfg', 'dfg', 'dfg', 'dfg', 'dgf', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 1);
+(17, 'sedg', 'dsfg', 'dfg', 'dfg', 'dfg', 'dgf', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 1),
+(18, 'new chrome case', 'new chrome case', 'new chrome case', 'new chrome case', 'new chrome case', 'new chrome case', 'active', 'start', 0, 0, 0, 0, 0, 0, 'notification', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dealer`
+-- Struktura tabeli dla tabeli `dealer`
 --
 
 CREATE TABLE `dealer` (
@@ -87,7 +88,7 @@ CREATE TABLE `dealer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `dealer`
+-- Zrzut danych tabeli `dealer`
 --
 
 INSERT INTO `dealer` (`id`, `name`, `street`, `zip_code`, `city`, `main_phone`, `main_fax`, `main_mail`, `phone_24h`, `phone_service_car`, `alt_phone_1`, `alt_phone_2`, `alt_mail_1`, `alt_mail_2`, `other_comments`, `is_active`) VALUES
@@ -103,7 +104,7 @@ INSERT INTO `dealer` (`id`, `name`, `street`, `zip_code`, `city`, `main_phone`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fos_user`
+-- Struktura tabeli dla tabeli `fos_user`
 --
 
 CREATE TABLE `fos_user` (
@@ -122,12 +123,12 @@ CREATE TABLE `fos_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `fos_user`
+-- Zrzut danych tabeli `fos_user`
 --
 
 INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
-(1, 'ccwrcadmin', 'ccwrcadmin', 'ccwrcadmin@gmail.elo', 'ccwrcadmin@gmail.elo', 1, NULL, '$2y$13$mdUZimf2vJ/q5o1SqQSRh.m6ldO29NGHlCcCuIsFjU1bdWNAT9w8u', '2017-08-07 17:20:33', NULL, NULL, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}'),
-(2, 'ccwrcoperator', 'ccwrcoperator', 'ccwrcoperator@gmail.elo', 'ccwrcoperator@gmail.elo', 1, NULL, '$2y$13$NPahNgRTTcYYeafyiKg0x.RS35r6nAu79N4pzKUj8ajLc5fH2EB4W', '2017-07-30 17:09:14', NULL, NULL, 'a:1:{i:0;s:13:\"ROLE_OPERATOR\";}'),
+(1, 'ccwrcadmin', 'ccwrcadmin', 'ccwrcadmin@gmail.elo', 'ccwrcadmin@gmail.elo', 1, NULL, '$2y$13$mdUZimf2vJ/q5o1SqQSRh.m6ldO29NGHlCcCuIsFjU1bdWNAT9w8u', '2017-09-04 08:33:42', NULL, NULL, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}'),
+(2, 'ccwrcoperator', 'ccwrcoperator', 'ccwrcoperator@gmail.elo', 'ccwrcoperator@gmail.elo', 1, NULL, '$2y$13$NPahNgRTTcYYeafyiKg0x.RS35r6nAu79N4pzKUj8ajLc5fH2EB4W', '2017-08-13 15:24:38', NULL, NULL, 'a:1:{i:0;s:13:\"ROLE_OPERATOR\";}'),
 (3, 'ccwrcuser', 'ccwrcuser', 'ccwrcuser@gmail.elo', 'ccwrcuser@gmail.elo', 1, NULL, '$2y$13$85Y1dC2YeX05aYoxrLAgOOmjsHKCaObP1t7c08ympJGrufzmX6lGe', '2017-07-26 14:46:43', NULL, NULL, 'a:0:{}'),
 (4, 'ccwrcdealer', 'ccwrcdealer', 'ccwrcdealer@ccwrcdealer.elo', 'ccwrcdealer@ccwrcdealer.elo', 1, NULL, '$2y$13$garsgYLil1hIyV7yu7dXGuHYGB2TELZgcca/wiUN4WdGN824meA2K', '2017-07-26 14:51:12', NULL, NULL, 'a:1:{i:0;s:11:\"ROLE_DEALER\";}'),
 (5, 'ccwrcsuperadmin', 'ccwrcsuperadmin', 'ccwrcsuperadmin@gmail.elo', 'ccwrcsuperadmin@gmail.elo', 1, NULL, '$2y$13$eFT7Nt2mA3zltKru5pmfJO5RtLGB7SrCgfUIBUcAy.TQFJsjKcNgO', '2017-07-26 14:54:32', NULL, NULL, 'a:1:{i:0;s:16:\"ROLE_SUPER_ADMIN\";}');
@@ -135,13 +136,12 @@ INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_
 -- --------------------------------------------------------
 
 --
--- Table structure for table `monitoring`
+-- Struktura tabeli dla tabeli `monitoring`
 --
 
 CREATE TABLE `monitoring` (
   `id` int(11) NOT NULL,
   `code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `code_description` varchar(600) COLLATE utf8_unicode_ci DEFAULT NULL,
   `time_save` datetime NOT NULL,
   `time_set` datetime DEFAULT NULL,
   `document` varchar(600) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -153,32 +153,69 @@ CREATE TABLE `monitoring` (
   `accident_case_id` int(11) DEFAULT NULL,
   `operator` varchar(600) COLLATE utf8_unicode_ci DEFAULT NULL,
   `home_dealer_id` int(11) DEFAULT NULL,
-  `repair_dealer_id` int(11) DEFAULT NULL
+  `repair_dealer_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `currency` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `monitoring`
+-- Zrzut danych tabeli `monitoring`
 --
 
-INSERT INTO `monitoring` (`id`, `code`, `code_description`, `time_save`, `time_set`, `document`, `contact_through`, `comments`, `out_comment`, `contact_mail`, `optional_mails`, `accident_case_id`, `operator`, `home_dealer_id`, `repair_dealer_id`) VALUES
-(4, 'START', 'opis kodu', '2017-08-01 00:00:00', NULL, NULL, 'kierowca 565656566', 'zgloszenie awarii', NULL, NULL, NULL, 1, 'ccwrcoperator', NULL, NULL),
-(5, 'PG', NULL, '2017-08-01 01:00:00', NULL, NULL, 'dealer 1', 'prośba o potwierdzenie wyplacalnosci', NULL, NULL, NULL, 1, 'ccwrcoperator', NULL, NULL),
-(6, 'CPG', NULL, '2017-08-01 02:00:00', NULL, NULL, 'dealer 1 jan kovalski 334444444', 'potwierdzil wyplacalnosc klienta', NULL, NULL, NULL, 1, 'ccwrcoperator', NULL, NULL),
-(7, 'testCode', 'sdfs', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'sdf', 'sdf', NULL, NULL, 'sdf', 1, 'sdf', NULL, NULL),
-(8, 'testCode', 'sad', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'asd', 'asd', NULL, NULL, 'asd', 1, 'asd', NULL, NULL),
-(9, 'testCode', 'etr', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'ert', 'ert', NULL, NULL, 'ert', 1, 'ccwrcadmin', NULL, NULL),
-(10, 'testCode', 'wre', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'wer', 'wre', NULL, NULL, 'wer', 1, 'ccwrcadmin', NULL, NULL),
-(11, 'testCode', 'ewrt', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'ert', 'ert', NULL, NULL, 'ert', 1, 'ccwrcadmin', NULL, NULL),
-(12, 'START', NULL, '2017-08-07 14:54:51', NULL, NULL, 'wer', 'werwerwrewrr', NULL, NULL, NULL, 15, 'ccwrcadmin', NULL, NULL),
-(13, 'START', NULL, '2017-08-07 14:57:23', NULL, NULL, 'new date test', 'new date test', NULL, NULL, NULL, 16, 'ccwrcadmin', NULL, NULL),
-(14, 'START', NULL, '2017-08-07 14:58:42', NULL, NULL, 'dfg', 'dfg', NULL, NULL, NULL, 17, 'ccwrcadmin', NULL, NULL),
-(15, 'testCode', 'PGtest', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'con tr', 'comment', NULL, NULL, 'opt mail', 17, 'ccwrcadmin', NULL, NULL),
-(16, 'yy', 'swfd', '2012-01-01 00:00:00', '2012-01-01 00:00:00', NULL, 'ads', 'asda', NULL, NULL, 'asd', 17, 'ccwrcadmin', NULL, NULL);
+INSERT INTO `monitoring` (`id`, `code`, `time_save`, `time_set`, `document`, `contact_through`, `comments`, `out_comment`, `contact_mail`, `optional_mails`, `accident_case_id`, `operator`, `home_dealer_id`, `repair_dealer_id`, `amount`, `currency`) VALUES
+(4, 'START', '2017-08-01 00:00:00', NULL, NULL, 'edit kierowca 565656566', 'edit zgloszenie awarii edit', NULL, NULL, NULL, 1, 'ccwrcoperator', NULL, NULL, NULL, NULL),
+(12, 'START', '2017-08-07 14:54:51', NULL, NULL, 'wer', 'werwerwrewrr', NULL, NULL, NULL, 15, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(13, 'START', '2017-08-07 14:57:23', NULL, NULL, 'edit new date test', 'edit new date test', NULL, NULL, NULL, 16, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(14, 'START', '2017-08-07 14:58:42', NULL, NULL, 'dfg', 'dfg', NULL, NULL, NULL, 17, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(27, 'START', '2017-08-09 16:57:52', NULL, NULL, 'new chrome case', 'new chrome case', NULL, NULL, NULL, 18, 'ccwrcoperator', NULL, NULL, NULL, NULL),
+(32, 'Incoming', '2017-08-13 17:16:27', NULL, NULL, 'inc', 'inc', NULL, NULL, NULL, 18, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(33, 'Incoming', '2017-08-14 11:27:14', NULL, NULL, 'incoming edit', 'incoming edit', NULL, NULL, NULL, 1, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(34, 'Incoming', '2017-08-14 12:18:34', NULL, NULL, 'fd ed', 'dfhg', NULL, NULL, NULL, 16, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(35, 'RO', '2017-08-15 12:10:59', NULL, NULL, 'sadf edit', 'sdf', 'sdf', NULL, NULL, 15, 'ccwrcadmin', 3, 7, NULL, NULL),
+(36, 'RO', '2017-08-15 12:47:48', NULL, NULL, 'case 18 edit', 'edit case 18', 'case 18', NULL, NULL, 18, 'ccwrcadmin', 1, 9, NULL, NULL),
+(37, 'ETA', '2017-08-17 10:32:59', '2018-08-17 10:00:00', NULL, 'new eta edit', 'new eta edit', NULL, NULL, NULL, 18, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(38, 'RO', '2017-08-17 10:49:27', NULL, NULL, 'asdg', 'sdgf', 'sfdg', NULL, NULL, 16, 'ccwrcadmin', 1, 6, NULL, NULL),
+(39, 'PG', '2017-08-17 11:33:55', NULL, NULL, 'df', 'dfg', 'dfg', NULL, NULL, 18, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(40, 'PG', '2017-08-17 11:39:40', NULL, NULL, 'wq3te', 'rtetr', 'ert', NULL, NULL, 1, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(41, 'CPG', '2017-08-17 11:40:03', NULL, NULL, 'sdfg', 'dfg', NULL, NULL, NULL, 1, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(42, 'RO', '2017-08-17 11:40:23', NULL, NULL, 'dfg', 'dfg', 'dfg', NULL, NULL, 1, 'ccwrcadmin', 1, 8, NULL, NULL),
+(43, 'ETA', '2017-08-17 11:41:33', '2017-08-17 22:24:00', NULL, 'sdg', 'dfg', NULL, NULL, NULL, 1, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(44, 'ETA', '2017-08-17 12:28:35', '2012-08-17 12:28:00', NULL, 'dsh edyta', 'ddfh', NULL, NULL, NULL, 1, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(45, 'STRR', '2017-08-21 12:30:41', '2017-08-21 12:19:00', NULL, 'edit start', 'start edit', NULL, NULL, NULL, 18, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(46, 'STRR', '2017-08-21 14:48:16', '2017-08-21 14:48:00', NULL, 'start', 'start 2', NULL, NULL, NULL, 7, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(47, 'Out', '2017-08-22 15:40:16', NULL, NULL, 'out', 'out', NULL, NULL, NULL, 7, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(48, 'Out', '2017-08-23 11:59:18', NULL, NULL, 'out edit', 'edit out', NULL, NULL, NULL, 3, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(49, 'END', '2017-08-23 17:39:25', '2017-08-23 17:39:00', NULL, 'end', 'end', NULL, NULL, NULL, 7, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(50, 'END', '2017-08-24 17:44:55', '2017-08-24 17:44:00', NULL, 'end', 'end', NULL, NULL, NULL, 6, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(51, 'END', '2017-08-27 11:50:06', '2017-08-27 11:49:00', NULL, 'end op', 'end op', NULL, NULL, NULL, 3, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(52, 'END', '2017-08-27 13:17:14', '2017-08-27 13:17:00', NULL, 'degf edit', 'dsfg edit', NULL, NULL, NULL, 18, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(53, 'END', '2017-08-27 15:30:04', '2017-08-27 15:29:00', NULL, 'dsgf edit', 'dfg edit', NULL, NULL, NULL, 2, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(54, 'PG', '2017-08-28 13:15:10', NULL, NULL, 'cd', 'dgf', 'dfg', NULL, NULL, 6, 'ccwrcadmin', 6, NULL, NULL, NULL),
+(55, 'PG', '2017-08-28 13:21:24', NULL, NULL, 'fbc', 'cvb', 'cv', NULL, NULL, 17, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(56, 'CPG', '2017-08-28 13:33:42', NULL, NULL, 'dsfff', 'dsfff', NULL, NULL, NULL, 16, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(57, 'RO', '2017-08-28 14:38:01', NULL, NULL, 'dfg edyta', 'dfg edyta', 'dfg', NULL, NULL, 16, 'ccwrcadmin', 1, 5, NULL, NULL),
+(58, 'ETA', '2017-08-29 11:08:20', '2017-08-29 11:08:00', NULL, 'new eta edot', 'edyut', NULL, NULL, NULL, 1, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(59, 'STRR', '2017-08-29 11:32:03', '2017-08-29 11:31:00', NULL, 'start edit', 'start edit', NULL, NULL, NULL, 15, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(60, 'Incoming', '2017-08-29 12:14:52', NULL, NULL, 'inc edit', 'inc edit', NULL, NULL, NULL, 15, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(61, 'Out', '2017-08-29 14:27:41', NULL, NULL, 'o e', 'e o', NULL, NULL, NULL, 6, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(62, 'END', '2017-08-30 17:43:56', '2017-08-30 17:43:00', NULL, 'dgf  ed', 'dfg  ed', NULL, NULL, NULL, 2, 'ccwrcadmin', NULL, NULL, NULL, NULL),
+(63, 'PG', '2017-08-31 14:39:13', NULL, NULL, 'df', 'dff', 'df', NULL, NULL, 2, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(64, 'WPG', '2017-08-31 14:41:18', NULL, NULL, 'sdf edit', 'sdf edit', 'sdf', NULL, NULL, 2, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(65, 'PG', '2017-09-01 13:15:45', NULL, NULL, 'f', 'f', 'f', NULL, NULL, 3, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(66, 'WPG', '2017-09-01 13:16:05', NULL, NULL, 'fe', 'fe', 'f', NULL, NULL, 3, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(67, 'CPG', '2017-09-01 14:46:03', NULL, NULL, 'saf', 'sdf', NULL, NULL, NULL, 17, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(68, 'WCPG', '2017-09-01 14:46:15', NULL, NULL, 'd', 'd', NULL, NULL, NULL, 17, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(69, 'WCPG', '2017-09-03 11:31:26', NULL, NULL, 'df edit', 'edit dfg', NULL, NULL, NULL, 1, 'ccwrcadmin', 1, NULL, NULL, NULL),
+(70, 'RO', '2017-09-03 11:59:52', NULL, NULL, 'gg', 'gg', 'gg', NULL, NULL, 4, 'ccwrcadmin', 1, 6, NULL, NULL),
+(71, 'RO', '2017-09-03 13:15:46', NULL, NULL, 'dxv', 'xcv', 'xcv', NULL, NULL, 3, 'ccwrcadmin', 1, 1, NULL, NULL),
+(72, 'WRO', '2017-09-03 13:15:57', NULL, NULL, 'xcv', 'cxvxc', 'vxcvx', NULL, NULL, 3, 'ccwrcadmin', 1, 1, NULL, NULL),
+(73, 'RO', '2017-09-04 08:43:54', NULL, NULL, 'r', 'r', 'r', NULL, NULL, 6, 'ccwrcadmin', 6, 7, NULL, NULL),
+(74, 'WRO', '2017-09-04 08:44:09', NULL, NULL, 'r edit', 'edit r', 'r', NULL, NULL, 6, 'ccwrcadmin', 6, 7, NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vehicle`
+-- Struktura tabeli dla tabeli `vehicle`
 --
 
 CREATE TABLE `vehicle` (
@@ -202,7 +239,7 @@ CREATE TABLE `vehicle` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `vehicle`
+-- Zrzut danych tabeli `vehicle`
 --
 
 INSERT INTO `vehicle` (`id`, `vin`, `street`, `city`, `phone`, `fax`, `mail`, `mileage`, `purchase_date`, `company_name`, `tax_id_number`, `contact_person`, `zip_code`, `registration_number`, `guarantee_type`, `name_type`, `dealer_id`) VALUES
@@ -219,7 +256,7 @@ INSERT INTO `vehicle` (`id`, `vin`, `street`, `city`, `phone`, `fax`, `mail`, `m
 (13, 'newvin12300', 'szcsd 2', 'wdeaxasx', '23423424', '121231', 'uyftdhtrd@uuu7.pl', '12333', '2017-01-01', 'sadfsEDIT', '234243', 'jukiu  4444444', '11-233', 'www1223e', 'empty', 'type 1 name 2', 3);
 
 --
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
@@ -251,9 +288,9 @@ ALTER TABLE `fos_user`
 --
 ALTER TABLE `monitoring`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_BA4F975D5A4A17E5` (`home_dealer_id`),
-  ADD UNIQUE KEY `UNIQ_BA4F975D65F38CB8` (`repair_dealer_id`),
-  ADD KEY `IDX_BA4F975DE7C7236B` (`accident_case_id`);
+  ADD KEY `IDX_BA4F975DE7C7236B` (`accident_case_id`),
+  ADD KEY `IDX_BA4F975D5A4A17E5` (`home_dealer_id`),
+  ADD KEY `IDX_BA4F975D65F38CB8` (`repair_dealer_id`);
 
 --
 -- Indexes for table `vehicle`
@@ -268,42 +305,42 @@ ALTER TABLE `vehicle`
 --
 
 --
--- AUTO_INCREMENT for table `accident_case`
+-- AUTO_INCREMENT dla tabeli `accident_case`
 --
 ALTER TABLE `accident_case`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
--- AUTO_INCREMENT for table `dealer`
+-- AUTO_INCREMENT dla tabeli `dealer`
 --
 ALTER TABLE `dealer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT for table `fos_user`
+-- AUTO_INCREMENT dla tabeli `fos_user`
 --
 ALTER TABLE `fos_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `monitoring`
+-- AUTO_INCREMENT dla tabeli `monitoring`
 --
 ALTER TABLE `monitoring`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 --
--- AUTO_INCREMENT for table `vehicle`
+-- AUTO_INCREMENT dla tabeli `vehicle`
 --
 ALTER TABLE `vehicle`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `accident_case`
+-- Ograniczenia dla tabeli `accident_case`
 --
 ALTER TABLE `accident_case`
   ADD CONSTRAINT `FK_56A83C38545317D1` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`id`);
 
 --
--- Constraints for table `monitoring`
+-- Ograniczenia dla tabeli `monitoring`
 --
 ALTER TABLE `monitoring`
   ADD CONSTRAINT `FK_BA4F975D5A4A17E5` FOREIGN KEY (`home_dealer_id`) REFERENCES `dealer` (`id`),
@@ -311,7 +348,7 @@ ALTER TABLE `monitoring`
   ADD CONSTRAINT `FK_BA4F975DE7C7236B` FOREIGN KEY (`accident_case_id`) REFERENCES `accident_case` (`id`);
 
 --
--- Constraints for table `vehicle`
+-- Ograniczenia dla tabeli `vehicle`
 --
 ALTER TABLE `vehicle`
   ADD CONSTRAINT `FK_1B80E486249E6EA1` FOREIGN KEY (`dealer_id`) REFERENCES `dealer` (`id`);
