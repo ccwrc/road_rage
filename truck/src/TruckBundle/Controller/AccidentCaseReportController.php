a8d89244cc29a1b7a52eff37d505b1fd5e87838e
ccwrc <j.muszelik@gmail.com>
2017-09-13 13:17:29 +0200

    add getDateDifferenceInMinutesOrReturnZero

--- truck/src/TruckBundle/Controller/AccidentCaseReportController.php
+++ truck/src/TruckBundle/Controller/AccidentCaseReportController.php
@@ -14,5 +14,18 @@ use \DateTime;
  * @Security("has_role('ROLE_OPERATOR')")
  */
 class AccidentCaseReportController extends AccidentCaseController {
-    
+
+    protected function getDateDifferenceInMinutesOrReturnZero($earlierDate, $laterDate) {
+        $diff = date_diff($earlierDate, $laterDate, false);
+        if ($diff->invert == 1) {
+            return 0;
+        }
+        // http://php.net/manual/pl/class.dateinterval.php
+//        $totalInMinutes = (($diff->y * 365.25 + $diff->m * 30 + $diff->d) * 24 + $diff->h) * 60 
+//                + $diff->i + $diff->s / 60;
+        $totalInMinutes = (($diff->y * 365.25 + $diff->m * 30 + $diff->d) * 24 + $diff->h) * 60 
+                + $diff->i;        
+        return (int) $totalInMinutes;
+    }
+
 }
