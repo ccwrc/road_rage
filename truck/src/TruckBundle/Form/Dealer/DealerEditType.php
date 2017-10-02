676fff4f999277604bbe1ea70c2ec5b41b891f1e
ccwrc <j.muszelik@gmail.com>
2017-10-02 11:27:54 +0200

    DealerEditType form - changes

--- truck/src/TruckBundle/Form/Dealer/DealerEditType.php
+++ truck/src/TruckBundle/Form/Dealer/DealerEditType.php
@@ -15,14 +15,35 @@ class DealerEditType extends AbstractType {
                 ->add("zipCode", "text", ["label" => "Zip code: "])
                 ->add("city", "text", ["label" => "City: "])
                 ->add("mainPhone", "text", ["label" => "Main phone: "])
-                ->add("mainFax", "text", ["label" => "Main fax: "])
+                ->add("mainFax", "text", [
+                    "label" => "Main fax: ",
+                    "required" => false
+                    ])
                 ->add("mainMail", "text", ["label" => "Main mail: "])
-                ->add("phone24h", "text", ["label" => "Phone - 24h: "])
-                ->add("phoneServiceCar", "text", ["label" => "Phone - service car: "])
-                ->add("altPhone1", "text", ["label" => "Phone - optional 1: "])
-                ->add("altPhone2", "text", ["label" => "Phone - optional 2: "])
-                ->add("altMail1", "text", ["label" => "Mail - optional 1: "])
-                ->add("altMail2", "text", ["label" => "Mail - optional 2: "])
+                ->add("phone24h", "text", [
+                    "label" => "Phone - 24h: ",
+                    "required" => false
+                    ])
+                ->add("phoneServiceCar", "text", [
+                    "label" => "Phone - service car: ",
+                    "required" => false
+                    ])
+                ->add("altPhone1", "text", [
+                    "label" => "Phone - optional 1: ",
+                    "required" => false
+                    ])
+                ->add("altPhone2", "text", [
+                    "label" => "Phone - optional 2: ",
+                    "required" => false
+                    ])
+                ->add("altMail1", "text", [
+                    "label" => "Mail - optional 1: ",
+                    "required" => false
+                    ])
+                ->add("altMail2", "text", [
+                    "label" => "Mail - optional 2: ",
+                    "required" => false
+                    ])
                 ->add("isActive", "choice", [
                     "choices" => [
                         "active" => "active",
@@ -30,7 +51,10 @@ class DealerEditType extends AbstractType {
                         "suspended" => "suspended"
                     ],
                     "choices_as_values" => true, "label" => "Is active: "])
-                ->add("otherComments", "textarea", ["label" => "Other comments: "]);
+                ->add("otherComments", "textarea", [
+                    "label" => "Other comments: ",
+                    "required" => false
+                    ]);
     }
 
     public function configureOptions(OptionsResolver $resolver) {
