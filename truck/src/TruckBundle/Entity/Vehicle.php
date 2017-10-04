f7db731d4465b5bc8bb45d8839d87f75c1033d4f
ccwrc <j.muszelik@gmail.com>
2017-10-04 14:35:02 +0200

    vehicle entity - add validator

--- truck/src/TruckBundle/Entity/Vehicle.php
+++ truck/src/TruckBundle/Entity/Vehicle.php
@@ -5,6 +5,8 @@ namespace TruckBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * Vehicle
  *
