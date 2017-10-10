7ccd0c4f68639b9b219d96e955c99a331a42f1c9
ccwrc <j.muszelik@gmail.com>
2017-10-10 10:05:19 +0200

    enable KnpSnappyBundle in AppKernel

--- truck/app/AppKernel.php
+++ truck/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel {
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new TruckBundle\TruckBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
