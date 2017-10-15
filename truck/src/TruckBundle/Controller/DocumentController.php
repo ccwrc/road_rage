10e83a0ad47c7e82efe421f00159f40512c7954d
ccwrc <j.muszelik@gmail.com>
2017-10-15 18:17:41 +0200

    create DocumentController

---
+++ truck/src/TruckBundle/Controller/DocumentController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TruckBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DocumentController extends Controller
+{
+}
---
+++ truck/src/TruckBundle/Tests/Controller/DocumentControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TruckBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DocumentControllerTest extends WebTestCase
+{
+}
