0bc06d31e86ba0dfc68d4a7fc05d5079f69a8a01
ccwrc <j.muszelik@gmail.com>
2017-12-22 13:40:51 +0100

    MonitoringCpgType - add amount and currency

--- truck/src/TruckBundle/Form/Monitoring/MonitoringCpgType.php
+++ truck/src/TruckBundle/Form/Monitoring/MonitoringCpgType.php
@@ -11,6 +11,15 @@ class MonitoringCpgType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add("contactThrough", "text", ["label" => "Contact through: "])
+                ->add("amount", "number", ["label" => "Amount: "])
+                ->add("currency", "choice", [
+                    "choices" => [
+                        "PLN" => "PLN",
+                        "USD" => "USD",
+                        "EUR" => "EUR"
+                    ],
+                    "choices_as_values" => true, "label" => "Currency: "
+                ])                
                 ->add("comments", "textarea", ["label" => "Comments: "]);
     }
 
