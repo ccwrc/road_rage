5fff65fb75f296b0ef77d35e74b85a7c63934c94
ccwrc <ccwrcltd@gmail.com>
2018-06-15 17:06:24 +0200

    added DealerFixtures

--- src/TruckBundle/DataFixtures/ORM/DealerFixtures.php
+++ src/TruckBundle/DataFixtures/ORM/DealerFixtures.php
@@ -17,13 +17,43 @@ class DealerFixtures extends Fixture implements DependentFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        for ($i=1; $i <= 50; $i++) {
+        for ($i = 1; $i <= 50; $i++) {
             $inactiveDealer = new Dealer();
             $inactiveDealer->setAltMail1($i . 'inactivedealer@pp.elo');
             $inactiveDealer->setAltPhone1('123456789');
             $inactiveDealer->setCity($i . 'city');
-            $inactiveDealer->setIsActive(Dealer::$active);
+            $inactiveDealer->setIsActive(Dealer::$inactive);
+            $inactiveDealer->setMainFax('123456789');
+            $inactiveDealer->setMainMail($i . 'inactivedealer@pp.1mail');
+            $inactiveDealer->setMainPhone($i . '12345678');
+            $inactiveDealer->setName('inactive Dealer' . $i);
+            $inactiveDealer->setOtherComments('plain comment' . $i);
+            $inactiveDealer->setPhone24h($i . '87654321');
+            $inactiveDealer->setPhoneServiceCar('12345699' . $i);
+            $inactiveDealer->setStreet('street' . $i);
+            $inactiveDealer->setZipCode(\mt_rand(10, 99) . '-225');
+            $manager->persist($inactiveDealer);
         }
+
+        for ($i = 51; $i <= 75; $i++) {
+            $activeDealer = new Dealer();
+            $activeDealer->setAltMail1($i . 'activedealer@pp.elo');
+            $activeDealer->setAltPhone1('123456789');
+            $activeDealer->setCity($i . 'city');
+            $activeDealer->setIsActive(Dealer::$active);
+            $activeDealer->setMainFax('123456789');
+            $activeDealer->setMainMail($i . 'activedealer@pp.1mail');
+            $activeDealer->setMainPhone($i . '12345678');
+            $activeDealer->setName('Active Dealer' . $i);
+            $activeDealer->setOtherComments('plain comment' . $i);
+            $activeDealer->setPhone24h($i . '87654321');
+            $activeDealer->setPhoneServiceCar('12345699' . $i);
+            $activeDealer->setStreet('street' . $i);
+            $activeDealer->setZipCode(\mt_rand(10, 99) . '-225');
+            $manager->persist($activeDealer);
+        }
+
+        $manager->flush();
     }
 
     public function getDependencies()
