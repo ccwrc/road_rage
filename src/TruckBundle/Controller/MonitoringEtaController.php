19efc0e4f784d860a2272241afac9b681785c57b
ccwrc <ccwrcltd@gmail.com>
2018-06-22 10:50:10 +0200

    MonitoringEtaController - small refactor

--- src/TruckBundle/Controller/MonitoringEtaController.php
+++ src/TruckBundle/Controller/MonitoringEtaController.php
@@ -7,6 +7,7 @@ namespace TruckBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use TruckBundle\Entity\Monitoring;
 use TruckBundle\Form\Monitoring\{
@@ -23,13 +24,15 @@ final class MonitoringEtaController extends MonitoringController
     /**
      * @Route("/{caseId}/createMonitoringEta", requirements={"caseId"="\d+"})
      */
-    public function createMonitoringEtaAction(Request $req, $caseId)
+    public function createMonitoringEtaAction(Request $req, int $caseId): Response
     {
         $case = $this->throwExceptionIfWrongIdOrGetCaseBy($caseId);
 
         $monitoringEta = new Monitoring();
-        $monitoringEta->setAccidentCase($case)->setOperator($this->getOperatorName())
-            ->setCode("ETA")->setTimeSet(new \DateTime("now"));
+        $monitoringEta->setAccidentCase($case)
+            ->setOperator($this->getOperatorName())
+            ->setCode(Monitoring::$codeEta)
+            ->setTimeSet(new \DateTime('now'));
         $form = $this->createForm(MonitoringEtaType::class, $monitoringEta);
 
         $form->handleRequest($req);
@@ -40,23 +43,23 @@ final class MonitoringEtaController extends MonitoringController
             $em->persist($monitoringEta);
             $em->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:create_monitoring_eta.html.twig', [
-            "form" => $form->createView(),
-            "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
 
     /**
      * @Route("/{monitoringId}/editMonitoringEta", requirements={"monitoringId"="\d+"})
      */
-    public function editMonitoringEtaAction(Request $req, $monitoringId)
+    public function editMonitoringEtaAction(Request $req, int $monitoringId): Response
     {
-        $monitoringEta = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, "ETA");
+        $monitoringEta = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, Monitoring::$codeEta);
         $caseId = $monitoringEta->getAccidentCase()->getId();
         $form = $this->createForm(MonitoringEtaEditType::class, $monitoringEta);
 
@@ -64,18 +67,16 @@ final class MonitoringEtaController extends MonitoringController
         if ($form->isSubmitted() && $form->isValid()) {
             $monitoringEta = $form->getData();
             $monitoringEta->setOperator($this->getOperatorName());
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
+            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:edit_monitoring_eta.html.twig', [
-            "form" => $form->createView(),
-            "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-
 }
