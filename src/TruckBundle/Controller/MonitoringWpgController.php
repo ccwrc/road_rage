cf55ee4120ddb6a9e92a8ca609d58c2e28cab821
ccwrc <ccwrcltd@gmail.com>
2018-06-22 17:21:07 +0200

    MonitoringWroController - small refactor

--- src/TruckBundle/Controller/MonitoringWpgController.php
+++ src/TruckBundle/Controller/MonitoringWpgController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
--- src/TruckBundle/Controller/MonitoringWroController.php
+++ src/TruckBundle/Controller/MonitoringWroController.php
@@ -1,26 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use TruckBundle\Entity\Monitoring;
-use TruckBundle\Form\Monitoring\MonitoringWroType;
-use TruckBundle\Form\Monitoring\MonitoringWroEditType;
+use TruckBundle\Form\Monitoring\{
+    MonitoringWroType, MonitoringWroEditType
+};
 
 /**
  * @Route("/monitoring")
  * @Security("has_role('ROLE_OPERATOR')")
  */
-class MonitoringWroController extends MonitoringController {
+final class MonitoringWroController extends MonitoringController
+{
 
     /**
      * @Route("/{monitoringRoId}/createMonitoringWro", requirements={"monitoringRoId"="\d+"})
      */
-    public function createMonitoringWroAction(Request $req, $monitoringRoId) {
-        $monitoringRo = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringRoId, "RO");
+    public function createMonitoringWroAction(Request $req, int $monitoringRoId): Response
+    {
+        $monitoringRo = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringRoId, Monitoring::$codeRo);
         $case = $monitoringRo->getAccidentCase();
         $caseId = $case->getId();
         $homeDealer = $monitoringRo->getHomeDealer();
@@ -31,54 +37,57 @@ class MonitoringWroController extends MonitoringController {
         $currency = $monitoringRo->getCurrency();
 
         $monitoringWro = new Monitoring();
-        $monitoringWro->setAccidentCase($case)->setOperator($this->getOperatorName())->setHomeDealer($homeDealer)
-                ->setCode("WRO")->setRepairDealer($repairDealer)->setContactMail($mailForDocument)
-                ->setOptionalMails($stringWithOptionalMails)->setAmount($amount)->setCurrency($currency);
+        $monitoringWro->setAccidentCase($case)
+            ->setOperator($this->getOperatorName())
+            ->setHomeDealer($homeDealer)
+            ->setCode(Monitoring::$codeWro)
+            ->setRepairDealer($repairDealer)
+            ->setContactMail($mailForDocument)
+            ->setOptionalMails($stringWithOptionalMails)
+            ->setAmount($amount)
+            ->setCurrency($currency);
         $form = $this->createForm(MonitoringWroType::class, $monitoringWro);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
             $this->setColorProgressRedForCase($case);
-            $monitoringWro = $form->getData();
             $em = $this->getDoctrine()->getManager();
             $em->persist($monitoringWro);
             $em->flush();
 
-            return $this->redirectToRoute("truck_documentwro_createandsendwro", [
-                        "monitoringWroId" => $monitoringWro->getId()
+            return $this->redirectToRoute('truck_documentwro_createandsendwro', [
+                'monitoringWroId' => $monitoringWro->getId()
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:create_monitoring_wro.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-    
+
     /**
      * @Route("/{monitoringId}/editMonitoringWro", requirements={"monitoringId"="\d+"})
      */
-    public function editMonitoringWroAction(Request $req, $monitoringId) {
-        $monitoringWro = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, "WRO");
+    public function editMonitoringWroAction(Request $req, int $monitoringId): Response
+    {
+        $monitoringWro = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, Monitoring::$codeWro);
         $caseId = $monitoringWro->getAccidentCase()->getId();
         $form = $this->createForm(MonitoringWroEditType::class, $monitoringWro);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
-            $monitoringWro = $form->getData();
             $monitoringWro->setOperator($this->getOperatorName());
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
+            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:edit_monitoring_wro.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-
 }
