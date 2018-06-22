9dd23c02b5cd75d341a882134212b02b9e14efba
ccwrc <ccwrcltd@gmail.com>
2018-06-22 13:04:57 +0200

    MonitoringOutController - small refactor

--- src/TruckBundle/Controller/MonitoringOutController.php
+++ src/TruckBundle/Controller/MonitoringOutController.php
@@ -1,73 +1,78 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use TruckBundle\Entity\Monitoring;
-use TruckBundle\Form\Monitoring\MonitoringOutType;
-use TruckBundle\Form\Monitoring\MonitoringOutEditType;
+use TruckBundle\Form\Monitoring\{
+    MonitoringOutType, MonitoringOutEditType
+};
 
 /**
  * @Route("/monitoring")
  * @Security("has_role('ROLE_OPERATOR')")
  */
-class MonitoringOutController extends MonitoringController {
+final class MonitoringOutController extends MonitoringController
+{
 
     /**
      * @Route("/{caseId}/createMonitoringOut", requirements={"caseId"="\d+"})
      */
-    public function createMonitoringOutAction(Request $req, $caseId) {
+    public function createMonitoringOutAction(Request $req, int $caseId): Response
+    {
         $case = $this->throwExceptionIfWrongIdOrGetCaseBy($caseId);
 
         $monitoringOut = new Monitoring();
-        $monitoringOut->setAccidentCase($case)->setOperator($this->getOperatorName())->setCode("Out");
+        $monitoringOut->setAccidentCase($case)
+            ->setOperator($this->getOperatorName())
+            ->setCode(Monitoring::$codeOut);
         $form = $this->createForm(MonitoringOutType::class, $monitoringOut);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
-            $monitoringOut = $form->getData();
             $em = $this->getDoctrine()->getManager();
             $em->persist($monitoringOut);
             $em->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:create_monitoring_out.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
 
     /**
      * @Route("/{monitoringId}/editMonitoringOut", requirements={"monitoringId"="\d+"})
      */
-    public function editMonitoringOutAction(Request $req, $monitoringId) {
-        $monitoringOut = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, "Out");
+    public function editMonitoringOutAction(Request $req, int $monitoringId): Response
+    {
+        $monitoringOut = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, Monitoring::$codeOut);
         $caseId = $monitoringOut->getAccidentCase()->getId();
         $form = $this->createForm(MonitoringOutEditType::class, $monitoringOut);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
-            $monitoringOut = $form->getData();
             $monitoringOut->setOperator($this->getOperatorName());
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
+            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:edit_monitoring_out.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-
 }
