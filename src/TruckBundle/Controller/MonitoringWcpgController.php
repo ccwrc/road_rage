4ab5198504e32a8081280ef07773c2ea107330dd
ccwrc <ccwrcltd@gmail.com>
2018-06-22 15:57:40 +0200

    MonitoringWcpgController - small refactor

--- src/TruckBundle/Controller/MonitoringWcpgController.php
+++ src/TruckBundle/Controller/MonitoringWcpgController.php
@@ -1,26 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use TruckBundle\Entity\Monitoring;
-use TruckBundle\Form\Monitoring\MonitoringWcpgType;
-use TruckBundle\Form\Monitoring\MonitoringWcpgEditType;
+use TruckBundle\Form\Monitoring\{
+    MonitoringWcpgType, MonitoringWcpgEditType
+};
 
 /**
  * @Route("/monitoring")
  * @Security("has_role('ROLE_OPERATOR')")
  */
-class MonitoringWcpgController extends MonitoringController {
+final class MonitoringWcpgController extends MonitoringController
+{
 
     /**
      * @Route("/{monitoringCpgId}/createMonitoringWcpg", requirements={"monitoringCpgId"="\d+"})
      */
-    public function createMonitoringWcpgAction(Request $req, $monitoringCpgId) {
-        $monitoringCpg = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringCpgId, "CPG");
+    public function createMonitoringWcpgAction(Request $req, int $monitoringCpgId): Response
+    {
+        $monitoringCpg = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringCpgId, Monitoring::$codeCpg);
         $case = $monitoringCpg->getAccidentCase();
         $caseId = $case->getId();
         $homeDealer = $monitoringCpg->getHomeDealer();
@@ -28,54 +34,54 @@ class MonitoringWcpgController extends MonitoringController {
         $currency = $monitoringCpg->getCurrency();
 
         $monitoringWcpg = new Monitoring();
-        $monitoringWcpg->setAccidentCase($case)->setOperator($this->getOperatorName())
-                ->setHomeDealer($homeDealer)->setCode("WCPG")->setAmount($amount)
-                ->setCurrency($currency);
+        $monitoringWcpg->setAccidentCase($case)
+            ->setOperator($this->getOperatorName())
+            ->setHomeDealer($homeDealer)
+            ->setCode(Monitoring::$codeWcpg)
+            ->setAmount($amount)
+            ->setCurrency($currency);
         $form = $this->createForm(MonitoringWcpgType::class, $monitoringWcpg);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
             $this->setColorProgressRedForCase($case);
-            $monitoringWcpg = $form->getData();
             $em = $this->getDoctrine()->getManager();
             $em->persist($monitoringWcpg);
             $em->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:create_monitoring_wcpg.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-    
+
     /**
      * @Route("/{monitoringId}/editMonitoringWcpg", requirements={"monitoringId"="\d+"})
      */
-    public function editMonitoringWcpgAction(Request $req, $monitoringId) {
-        $monitoringWcpg = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, "WCPG");
+    public function editMonitoringWcpgAction(Request $req, int $monitoringId): Response
+    {
+        $monitoringWcpg = $this->throwExceptionIfHasWrongDataOrGetMonitoringBy($monitoringId, Monitoring::$codeWcpg);
         $caseId = $monitoringWcpg->getAccidentCase()->getId();
         $form = $this->createForm(MonitoringWcpgEditType::class, $monitoringWcpg);
 
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
-            $monitoringWcpg = $form->getData();
             $monitoringWcpg->setOperator($this->getOperatorName());
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
+            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute("truck_operator_panel", [
-                        "caseId" => $caseId
+            return $this->redirectToRoute('truck_operator_panel', [
+                'caseId' => $caseId
             ]);
         }
 
         return $this->render('TruckBundle:Monitoring:edit_monitoring_wcpg.html.twig', [
-                    "form" => $form->createView(),
-                    "caseId" => $caseId
+            'form' => $form->createView(),
+            'caseId' => $caseId
         ]);
     }
-
 }
