9a28163d67f981891efa8442b4d76bcf3a51782a
ccwrc <ccwrcltd@gmail.com>
2018-07-03 18:28:35 +0200

    MonitoringCpgType - refactor

--- src/TruckBundle/Form/Monitoring/MonitoringCpgType.php
+++ src/TruckBundle/Form/Monitoring/MonitoringCpgType.php
@@ -1,32 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Form\Monitoring;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{
+    ChoiceType, NumberType, TextareaType, TextType
+};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MonitoringCpgType extends AbstractType {
+use TruckBundle\Entity\Monitoring;
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+class MonitoringCpgType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
-                ->add("amount", "number", ["label" => "Amount: "])
-                ->add("currency", "choice", [
-                    "choices" => [
-                        "PLN" => "PLN",
-                        "USD" => "USD",
-                        "EUR" => "EUR"
-                    ],
-                    "choices_as_values" => true, "label" => "Currency: "
-                ])    
-                ->add("contactThrough", "text", ["label" => "Contact through: "])
-                ->add("comments", "textarea", ["label" => "Comments: "]);
+            ->add('amount', NumberType::class, [
+                'label' => 'Amount: '
+            ])
+            ->add('currency', ChoiceType::class, [
+                'choices' => [
+                    'PLN' => Monitoring::$currencyPln,
+                    'USD' => Monitoring::$currencyUsd,
+                    'EUR' => Monitoring::$currencyEur
+                ],
+                'choices_as_values' => true,
+                'label' => 'Currency: '
+            ])
+            ->add('contactThrough', TextType::class, [
+                'label' => 'Contact through: '
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => 'Comments: '
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver): void
+    {
         $resolver->setDefaults([
-            "data_class" => "TruckBundle\Entity\Monitoring"
+            'data_class' => 'TruckBundle\Entity\Monitoring'
         ]);
     }
-
 }
