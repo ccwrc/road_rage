ff51b32b26e826ea192a03bc67be87700baaefb6
ccwrc <ccwrcltd@gmail.com>
2018-07-03 20:39:48 +0200

    MonitoringPgType - refactor

--- src/TruckBundle/Form/Monitoring/MonitoringPgType.php
+++ src/TruckBundle/Form/Monitoring/MonitoringPgType.php
@@ -1,40 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Form\Monitoring;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{
+    ChoiceType, NumberType, TextareaType, TextType
+};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MonitoringPgType extends AbstractType {
+use TruckBundle\Entity\Monitoring;
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+class MonitoringPgType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
-                ->add("contactThrough", "text", ["label" => "Contact through: "])
-                ->add("comments", "textarea", ["label" => "Comments: "])
-                ->add("amount", "number", ["label" => "Amount: "])
-                ->add("currency", "choice", [
-                    "choices" => [
-                        "PLN" => "PLN",
-                        "USD" => "USD",
-                        "EUR" => "EUR"
-                    ],
-                    "choices_as_values" => true, "label" => "Currency: "
-                ])
-                ->add("outComment", "textarea", [
-                    "label" => "Comment for dealer: ",
-                    "required" => false
-                ])
-                ->add("optionalMails", "textarea", [
-                    "label" => "Optional mails to send the document: ",
-                    "required" => false
-        ]);
+            ->add('contactThrough', TextType::class, [
+                'label' => 'Contact through: '
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => 'Comments: '
+            ])
+            ->add('amount', NumberType::class, [
+                'label' => 'Amount: '
+            ])
+            ->add('currency', ChoiceType::class, [
+                'choices' => [
+                    'PLN' => Monitoring::$currencyPln,
+                    'USD' => Monitoring::$currencyUsd,
+                    'EUR' => Monitoring::$currencyEur
+                ],
+                'choices_as_values' => true,
+                'label' => 'Currency: '
+            ])
+            ->add('outComment', TextareaType::class, [
+                'label' => 'Comment for dealer: ',
+                'required' => false
+            ])
+            ->add('optionalMails', TextareaType::class, [
+                'label' => 'Optional mails to send the document: ',
+                'required' => false
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver): void
+    {
         $resolver->setDefaults([
-            "data_class" => "TruckBundle\Entity\Monitoring"
+            'data_class' => 'TruckBundle\Entity\Monitoring'
         ]);
     }
-
 }
