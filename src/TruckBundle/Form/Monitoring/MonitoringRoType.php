57b113c5fc026ce36e32c18b222a388845324546
ccwrc <ccwrcltd@gmail.com>
2018-07-03 20:53:35 +0200

    MonitoringRoType - refactor

--- src/TruckBundle/Form/Monitoring/MonitoringRoType.php
+++ src/TruckBundle/Form/Monitoring/MonitoringRoType.php
@@ -1,54 +1,70 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Form\Monitoring;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType; // for dealer
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{
+    ChoiceType, NumberType, TextareaType, TextType
+};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-use TruckBundle\Repository\DealerRepository; // for query_builder
+use TruckBundle\Entity\Dealer;
+use TruckBundle\Entity\Monitoring;
+use TruckBundle\Repository\DealerRepository;
 
-class MonitoringRoType extends AbstractType {
+class MonitoringRoType extends AbstractType
+{
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
-                ->add("contactThrough", "text", ["label" => "Contact through: "])
-                ->add("comments", "textarea", ["label" => "Comments: "])
-                ->add("amount", "number", ["label" => "Amount: "])
-                ->add("currency", "choice", [
-                    "choices" => [
-                        "PLN" => "PLN",
-                        "USD" => "USD",
-                        "EUR" => "EUR"
-                    ],
-                    "choices_as_values" => true, "label" => "Currency: "
-                ])                
-                ->add("outComment", "textarea", [
-                    "label" => "Comment for repair dealer: ",
-                    "required" => false
-                ])
-                ->add("optionalMails", "textarea", [
-                    "label" => "Optional mails to send the document: ",
-                    "required" => false
-                ])
-                ->add("repairDealer", EntityType::class, [
-                    "class" => "TruckBundle:Dealer",
-                    "query_builder" => function (DealerRepository $dr) {
-                        return $dr->createQueryBuilder('dealer')
-                                ->where('dealer.isActive = :active')
-                                ->orderBy('dealer.name', 'ASC')
-                                ->setParameter("active", "active");
-                    },
-                    "choice_label" => "name",
-                    "label" => "Repair dealer: "
-        ]);
+            ->add('contactThrough', TextType::class, [
+                'label' => 'Contact through: '
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => 'Comments: '
+            ])
+            ->add('amount', NumberType::class, [
+                'label' => 'Amount: '
+            ])
+            ->add('currency', ChoiceType::class, [
+                'choices' => [
+                    'PLN' => Monitoring::$currencyPln,
+                    'USD' => Monitoring::$currencyUsd,
+                    'EUR' => Monitoring::$currencyEur
+                ],
+                'choices_as_values' => true,
+                'label' => 'Currency: '
+            ])
+            ->add('outComment', TextareaType::class, [
+                'label' => 'Comment for repair dealer: ',
+                'required' => false
+            ])
+            ->add('optionalMails', TextareaType::class, [
+                'label' => 'Optional mails to send the document: ',
+                'required' => false
+            ])
+            ->add('repairDealer', EntityType::class, [
+                'class' => 'TruckBundle:Dealer',
+                'query_builder' => function (DealerRepository $dr) {
+                    return $dr->createQueryBuilder('dealer')
+                        ->where('dealer.isActive = :active')
+                        ->orderBy('dealer.name', 'ASC')
+                        ->setParameter('active', Dealer::$dealerIsActive);
+                },
+                'choice_label' => 'name',
+                'label' => 'Repair dealer: '
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver): void
+    {
         $resolver->setDefaults([
-            "data_class" => "TruckBundle\Entity\Monitoring"
+            'data_class' => 'TruckBundle\Entity\Monitoring'
         ]);
     }
-
 }
