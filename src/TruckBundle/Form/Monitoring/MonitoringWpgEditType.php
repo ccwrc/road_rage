5461fd0d05394cd144c2e9721c347f2e0c47162e
ccwrc <ccwrcltd@gmail.com>
2018-07-05 15:40:31 +0200

    MonitoringWpgEditType - refactor

--- src/TruckBundle/Form/Monitoring/MonitoringWpgEditType.php
+++ src/TruckBundle/Form/Monitoring/MonitoringWpgEditType.php
@@ -1,27 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Form\Monitoring;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MonitoringWpgEditType extends AbstractType {
+class MonitoringWpgEditType extends AbstractType
+{
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
-                ->add("contactThrough", "text", ["label" => "Contact through: "])
-                ->add("comments", "textarea", ["label" => "Comments: "])
-                ->add("outComment", "textarea", [
-                    "label" => "Comment for dealer (the document has already been sent): ",
-                    "read_only" => true
-                    ]);                       
+            ->add('contactThrough', TextType::class, [
+                'label' => 'Contact through: '
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => 'Comments: '
+            ])
+            ->add('outComment', TextareaType::class, [
+                'label' => 'Comment for dealer (the document has already been sent): ',
+                'read_only' => true
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver): void
+    {
         $resolver->setDefaults([
-            "data_class" => "TruckBundle\Entity\Monitoring"
+            'data_class' => 'TruckBundle\Entity\Monitoring'
         ]);
     }
-
 }
