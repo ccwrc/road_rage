5a861837499f4ff566a2530a969091e045c22814
ccwrc <ccwrcltd@gmail.com>
2018-07-05 16:25:59 +0200

    NoteType - refactor

--- src/TruckBundle/Form/Note/NoteType.php
+++ src/TruckBundle/Form/Note/NoteType.php
@@ -1,30 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TruckBundle\Form\Note;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{
+    ChoiceType, DateTimeType, TextareaType
+};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use DateTime;
 
-class NoteType extends AbstractType {
+use TruckBundle\Entity\Note;
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+class NoteType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
-                ->add("status", "choice", [
-                    "choices" => [
-                        "private" => "private",
-                        "PUBLIC" => "public"
-                    ],
-                    "choices_as_values" => true, "label" => "Note status: "])
-                ->add("content", "textarea", ["label" => "Note content: "])
-                ->add("timePublication", "datetime", ["label" => "Time of publication: "]);
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'private' => Note::$statusPrivate,
+                    'PUBLIC' => Note::$statusPublic
+                ],
+                'choices_as_values' => true,
+                'label' => 'Note status: '
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Note content: '
+            ])
+            ->add('timePublication', DateTimeType::class, [
+                'label' => 'Time of publication: '
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver): void
+    {
         $resolver->setDefaults([
-            "data_class" => "TruckBundle\Entity\Note"
+            'data_class' => 'TruckBundle\Entity\Note'
         ]);
     }
-
 }
